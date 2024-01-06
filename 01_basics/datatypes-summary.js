d22261d3d3a94b7f3d62409a72a6e21188260f89
Udita <153092248+udita-0707@users.noreply.github.com>
2024-01-06 03:01:13 +0000

    update

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -19,15 +19,36 @@
 // JavaScript is a dynamically typed language, which means that data types of variables are determined by the value 
 // they hold at runtime and can change throughout the program as we assign different values to them.
 
-const heros = ["SHAKTIMAAN", "NAAGRAJ", "DOGA"]
-let MyObj = {
-    name: "Udita",
-    age: 19,
+// const heros = ["SHAKTIMAAN", "NAAGRAJ", "DOGA"]
+// let MyObj = {
+//     name: "Udita",
+//     age: 19,
+// }
+// // values in curly braces are object type and can be
+// // function can be treated as variable in javascript
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+// console.log(typeof heros); // output object
+// console.log(typeof myFunction); // output function (function object)
+
+// +++++++++++++++++++++++++ Memories ++++++++++++++++++++++
+// Stack(Primitive(kisi bhi variable ka copy milta hai)), Heap (Non-primitive(original value ka reference milta hai))
+// let myYoutubename = "hiteshchoudharydotcom"
+// let anothername = myYoutubename
+// console.log(anothername);
+// yahan copy mil rahi thi original ki
+// let myYoutubename = "hiteshchoudharydotcom"
+// let anothername = "ChaiaurCode"
+// console.log(myYoutubename);
+// console.log(anothername);
+
+let userOne = {
+    email:"user@google.com",
+    upi: "user@ybl"
 }
-// values in curly braces are object type and can be
-// function can be treated as variable in javascript
-const myFunction = function(){
-    console.log("Hello World");
-}
-console.log(typeof heros); // output object
-console.log(typeof myFunction); // output function
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+// yahan original value change krdete hai
