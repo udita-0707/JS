ea0c278510157504e6da5c74e5665c9f1ace5802
Udita <153092248+udita-0707@users.noreply.github.com>
2024-01-09 08:48:50 +0000

    revised

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -16,6 +16,10 @@ accountCity = "Bengaluru"
 /*
 Prefer not to use var because of issue in block scope and functional scope
 */
+/*
+Function Scope: When a variable is declared inside a function, it is only accessible within that function and cannot be used outside that function. 
+Block Scope: A variable when declared inside the if or switch conditions or inside for or while loops, are accessible within that particular condition or loop.
+*/
 
 console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
